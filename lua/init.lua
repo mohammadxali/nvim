f8a885b8c5087a78c6a2a6a681a951a57f13292d
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2023-07-30 00:18:01 +0330

    refactor: move `logo` to `config`

--- lua/init.lua
+++ lua/init.lua
@@ -69,23 +69,7 @@ require("lazy").setup({
         event = "VimEnter",
         opts = function()
             local dashboard = require("alpha.themes.dashboard")
-            local logo = [[
-                                         _.oo.
-                _.u[[/;:,.         .odMMMMMM'
-              .o888UU[[[/;:-.  .o@P^    MMM^
-             oN88888UU[[[/;::-.        dP^
-            dNMMNN888UU[[[/;:--.   .o@P^
-           ,MMMMMMN888UU[[/;::-. o@^
-           NNMMMNN888UU[[[/~.o@P^
-           888888888UU[[[/o@^-..
-          oI8888UU[[[/o@P^:--..
-       .@^  YUU[[[/o@^;::---..
-     oMP     ^/o@P^;:::---..
-  .dMMM    .o@^ ^;::---...
- dMMMMMMM@^`       `^^^^
-YMMMUP^
- ^^
-      ]]
+            local logo = require("config").logo
 
             dashboard.section.header.val = vim.split(logo, "\n")
             dashboard.section.buttons.val = {
