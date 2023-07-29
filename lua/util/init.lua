fdb9e8b689bc554ab0e6013b37002e0a35e018e7
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2023-07-30 01:00:15 +0330

    feat: setup `nvim-navic` to show code context in `lualine`

--- lua/util/init.lua
+++ lua/util/init.lua
@@ -2,6 +2,17 @@ local M = {}
 
 M.root_patterns = { ".git", "lua" }
 
+---@param on_attach fun(client, buffer)
+function M.on_attach(on_attach)
+    vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+            local buffer = args.buf
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            on_attach(client, buffer)
+        end,
+    })
+end
+
 function M.fg(name)
     local hl = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, { name = name }) or vim.api.nvim_get_hl_by_name(name, true)
     local fg = hl and hl.fg or hl.foreground
