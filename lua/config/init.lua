ac16bfbd471cf8b5d723bf81042f3b6061ee55cc
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2023-07-30 00:10:55 +0330

    feat: added `lualine.nvim` integration into plugins

---
+++ lua/config/init.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+M.icons = {
+    diagnostics = {
+        Error = " ",
+        Warn = " ",
+        Hint = " ",
+        Info = " ",
+    },
+    git = {
+        added = " ",
+        modified = " ",
+        removed = " ",
+    },
+}
+
+return M
---
+++ lua/util/init.lua
@@ -0,0 +1,11 @@
+local M = {}
+
+M.root_patterns = { ".git", "lua" }
+
+function M.fg(name)
+    local hl = vim.api.nvim_get_hl and vim.api.nvim_get_hl(0, { name = name }) or vim.api.nvim_get_hl_by_name(name, true)
+    local fg = hl and hl.fg or hl.foreground
+    return fg and { fg = string.format("#%06x", fg) }
+end
+
+return M
