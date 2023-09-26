cd9c977bc9dea4c3a5bf444e68644e8aa8379fef
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2023-09-26 15:47:28 +0330

    chore: added `vim-eunuch` plugin

--- lua/init.lua
+++ lua/init.lua
@@ -241,12 +241,12 @@ require("lazy").setup({
         end,
         cond = isNvimOnly
     },
-})
 
-vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
-vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
-vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
-vim.keymap.set({ "n", "o", "x" }, "ge", "<cmd>lua require('spider').motion('ge')<CR>", { desc = "Spider-ge" })
+    {
+        "tpope/vim-eunuch",
+        cond = isNvimOnly
+    },
+})
 
 if isNvimOnly then
     -- Colorscheme
