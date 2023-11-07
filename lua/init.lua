8e9943dfece8527ed7828daa6b7cc9c466cabdb7
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2023-11-07 13:12:21 +0330

    feat: add `wilder.nvim` support

--- lua/init.lua
+++ lua/init.lua
@@ -45,6 +45,11 @@ require("lazy").setup({
     },
 
     {
+        'gelguy/wilder.nvim',
+        cond = isNvimOnly,
+    },
+
+    {
         'nvim-treesitter/nvim-treesitter',
         cond = isNvimOnly
     },
@@ -318,4 +323,41 @@ if isNvimOnly then
     -- We already have diagnostics virtual text so there is no need for this
     -- Since these signs replace the hunks signs (+ | ~ | -) let's disable them
     vim.diagnostic.config({ signs = false })
+
+    -- Wilder
+    local wilder = require('wilder')
+    wilder.setup({ modes = { ':' }, next_key = '<C-j>', previous_key = '<C-k>', })
+
+    wilder.set_option('renderer', wilder.popupmenu_renderer(
+        wilder.popupmenu_palette_theme({
+            -- 'single', 'double', 'rounded' or 'solid'
+            -- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
+            border = 'rounded',
+            max_height = '75%',        -- max height of the palette
+            min_height = 0,            -- set to the same as 'max_height' for a fixed height window
+            prompt_position = 'top',   -- 'top' or 'bottom' to set the location of the prompt
+            reverse = 0,               -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
+            highlighter = {
+                wilder.lua_pcre2_highlighter(), -- requires `luarocks install pcre2`
+                wilder.lua_fzy_highlighter(), -- requires fzy-lua-native vim plugin found
+                -- at https://github.com/romgrk/fzy-lua-native
+            },
+            highlights = {
+                accent = wilder.make_hl('WilderAccent', 'Pmenu', { { a = 1 }, { a = 1 }, { foreground = '#f4468f' } }),
+            },
+        })
+    ))
+
+    wilder.set_option('pipeline', {
+        wilder.branch(
+            wilder.cmdline_pipeline({
+                -- sets the language to use, 'vim' and 'python' are supported
+                language = 'vim',
+                -- 0 turns off fuzzy matching
+                -- 1 turns on fuzzy matching
+                -- 2 partial fuzzy matching (match does not have to begin with the same first letter)
+                fuzzy = 1,
+            })
+        ),
+    })
 end
