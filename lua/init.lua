ff9e5e1be80846252aaa9505fb3462f1515eb77f
Mohammad Ali Shahbazi <mohammadali.shahbazi@gmail.com>
2024-01-27 10:13:33 +0330

    style: reformat file

--- lua/init.lua
+++ lua/init.lua
@@ -365,13 +365,13 @@ if isNvimOnly then
             -- 'single', 'double', 'rounded' or 'solid'
             -- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
             border = 'rounded',
-            max_height = '75%',        -- max height of the palette
-            min_height = 0,            -- set to the same as 'max_height' for a fixed height window
-            prompt_position = 'top',   -- 'top' or 'bottom' to set the location of the prompt
-            reverse = 0,               -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
+            max_height = '75%',                 -- max height of the palette
+            min_height = 0,                     -- set to the same as 'max_height' for a fixed height window
+            prompt_position = 'top',            -- 'top' or 'bottom' to set the location of the prompt
+            reverse = 0,                        -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
             highlighter = {
                 wilder.lua_pcre2_highlighter(), -- requires `luarocks install pcre2`
-                wilder.lua_fzy_highlighter(), -- requires fzy-lua-native vim plugin found
+                wilder.lua_fzy_highlighter(),   -- requires fzy-lua-native vim plugin found
                 -- at https://github.com/romgrk/fzy-lua-native
             },
             highlights = {
